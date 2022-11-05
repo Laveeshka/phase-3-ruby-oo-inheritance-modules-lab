21366ac5c25d74971da956ce0673fc54cff1269d
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-11-06 07:20:55 +1100

    use nested modules to refactor repetitive code

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,31 +1,36 @@
 require 'pry'
-
+# when we include/extend modules in our class, we tell the class to inherit methods from those modules
 class Artist
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+
   attr_accessor :name
   attr_reader :songs
 
   @@artists = []
 
   def initialize
-    @@artists << self
+    super #execute any code placed inside the super class' method of the same name (found in nested ClassMethods module in Memorable module), and then execute any code inside the child class' method
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@artists.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    self.all.count
-  end
+  # def self.count
+  #   self.all.count
+  # end
 
   def add_song(song)
     @songs << song
@@ -36,7 +41,7 @@ class Artist
     songs.each { |song| add_song(song) }
   end
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
 end
